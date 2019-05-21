18dc43f92629c41a92015cb55825f6508e7ff24b
ILYA CHUB <iliachubich@gmail.com>
2019-05-21 20:32:38 +0300

    Создана программа

--- program.py
+++ program.py
@@ -6,4 +6,23 @@ def add_random_list (list=[]):
     print("\n")
     return list
 
+def reverse(list, K, M):
+    if M==K:
+        print(list)
+        return list
+    if M-K==1:
+        k=list[K]
+        list[K]=list[M]
+        list[M]=k
+        print(list)
+        return list
+    k=list[K]
+    list[K]=list[M]
+    list[M]=k
+    return reverse(list,K+1,M-1)
+    reverse(list, K+1)
+
 list=add_random_list()
+K=int(input("Введите номер первого элемента в массиве дле ревераса: "))
+M=int(input("Введите номер последнего элемента в массиве дле ревераса: "))
+reverse(list,K,M)
